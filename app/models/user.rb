fd2c839d23c634ad27c78aae66903d09a6b731b2
Travis Smith <mr_travis_smith@hotmail.com>
2016-11-03 20:10:00 +0000

    add user_stocks, display on portfolio and delete action for current users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :user_stocks
+  has_many :stocks, through: :user_stocks
+
+  def can_add_stock?(ticker_symbol)
+    has_fewer_than_10_stocks? && !stock_already_added?(ticker_symbol)
+  end
+  
+  def has_fewer_than_10_stocks?
+    (user_stocks.count) < 10
+  end
+  
+  def stock_already_added?(ticker_symbol)
+    stock = Stock.find_by_ticker(ticker_symbol)
+    return false unless stock
+    user_stocks.where(stock_id: stock.id).exists?
+  end
+  
 end
+
